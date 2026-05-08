bec64ea51f4a1df4ca9f557c715f4db208051494
Vyshu-jessy <vyshnavipatoju@gmail.com>
2026-05-08 11:32:43 +0530

    My calci code

---
+++ Calculator.java
@@ -0,0 +1,18 @@
+class Calc{
+    public int add(int a,int b){
+        return a+b;
+    }
+}
+class AdvCalc extends Calc{
+    public int add(int a,int b){
+        return a+b+3;
+    }
+}
+public class Calculator{
+    public static void main(String[] args){
+        Calc c=new Calc();
+        System.out.println(c.add(2,3));
+        AdvCalc a=new AdvCalc();
+        System.out.println(a.add(2,3));
+    }
+}
